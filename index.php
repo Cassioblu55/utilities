8064f090c5cdaf8901ed33e2a6cce78c65915550
Cassio Hudson <cassioblubyrd@gmail.com>
2016-07-20 02:01:53 -0500

    Fixed typo

--- index.php
+++ index.php
@@ -8,8 +8,6 @@
 
 <header>
 	<h1>List of working utilities found within this project</h1>
-
-
 </header>
 
 <main>
@@ -17,18 +15,10 @@
 		<a href="examples/clickableTabs.php">Clickable Tab Creator</a>
 	</div>
 	<div>
-		<a href="examples/errorDisplay.php?error=This is an Error Message">Disaply error message</a>
+		<a href="examples/errorDisplay.php?error=This is an Error Message">Display error message</a>
 	</div>
 
 </main>
-
-
-<script src="js/clickableTabs.js"></script>
-
-<script type="text/javascript">
-	createClickableTabs("test2");
-</script>
-
 
 </body>
 
